490ecc0a104ba490a675a0a7b3ec5143474c287c
Marta Karbowska <karbowana.marta@gmail.com>
2022-02-12 13:34:00 +0100

    Connected books API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,71 @@
 <template>
   <div class="home">
-    <h1 class="heading">Get Book</h1>
+    <div class="search">
+      <input
+        type="text"
+        class="search__input"
+        id="search__input"
+        v-model="searchValue"
+        @input="handleInput"
+      />
+      <ul class="list">
+        <li class="list__item" v-for="item in results" :key="item.key">
+          {{ item.title }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import axios from "axios";
+import { debounce } from "lodash";
+
 export default {
   name: "Home",
+  data() {
+    return {
+      searchValue: "",
+      results: [],
+    };
+  },
+
+  methods: {
+    handleInput: debounce(function (this: any) {
+      axios
+        .get(
+          `https://openlibrary.org/search.json?q=${this.searchValue}&_facet=false&_spellcheck_count=0&limit=10&fields=key,cover_i,title,author_name,name&mode=everything`
+        )
+        .then((response) => {
+          this.results = response.data.docs;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  margin: 0;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.search {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+
+  &__input {
+    height: 30px;
+    border: 0;
+    border-bottom: 1px solid black;
+  }
+}
+</style>
