1313a272b0fd009a4e822ed93dbf3527f56882d9
Marta Karbowska <karbowana.marta@gmail.com>
2022-02-15 15:11:30 +0100

    Added new component with background

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="backgroundImageWrapper">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackgroundImage",
+};
+</script>
+
+<style lang="scss" scoped>
+.backgroundImageWrapper {
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background-image: url("../assets/books-gb5e5e6791_1920.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 7% 0%;
+  padding: 8px 30px;
+}
+
+.backgroundImageWrapper::after {
+  content: "";
+  background-color: rgb(245, 245, 245);
+  opacity: 0.4;
+  position: absolute;
+  z-index: 2;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+}
+</style>
