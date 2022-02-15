45f5f6ec907c716cf25ab60924beb5e1f94dd935
Marta Karbowska <karbowana.marta@gmail.com>
2022-02-15 15:16:00 +0100

    Added styles for input

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -18,16 +18,29 @@ export default {
 
 <style lang="scss" scoped>
 .searchWrapper {
-  width: 300px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
   margin-top: 22px;
 }
 
 .input {
   height: 30px;
+  width: 250px;
   border: 0;
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid rgb(66, 11, 66);
   background: none;
+  z-index: 3;
+  font-size: 18px;
+  font-weight: 400;
+  text-align: center;
+  color: rgb(66, 11, 66);
+  transition: box-shadow 0.5s;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 8px 18px -10px rgba(82, 40, 82, 0.6);
+  }
 }
 </style>
